c2c11400212a3b471cf19fff32b75f67b7211640
Lucas H. Xu <xuh@bu.edu>
2017-06-12 14:50:39 -0400

    change the security group name

--- service/instance.py
+++ service/instance.py
@@ -1356,7 +1356,7 @@ def user_delete_security_group(core_identity):
 
 def security_group_init(core_identity, max_attempts=3):
     has_secret = core_identity.get_credential('secret') is not None
-    security_group_name = core_identity.provider.get_config("network", "security_group_name", "default")
+    security_group_name = core_identity.provider.get_config("network", "security_group_name", "GIJI")
     if has_secret:
         return admin_security_group_init(core_identity)
     return user_security_group_init(core_identity, security_group_name = security_group_name)
