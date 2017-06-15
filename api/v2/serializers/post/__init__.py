072e7d19ad1bebfb77d2c2ae98d1d86f22c0674d
Lucas H. Xu <xuh@bu.edu>
2017-06-15 16:01:35 -0400

    clean up a typo

--- api/v2/serializers/post/__init__.py
+++ api/v2/serializers/post/__init__.py
@@ -1,7 +1,6 @@
 from .instance import InstanceSerializer
 from .account import AccountSerializer
 from .token_update import TokenUpdateSerializer
-from .moc_token_update import MOCTokenUpdateSerializer
 from .provider import ProviderSerializer
 from .volume import VolumeSerializer
 from .sahara_cluster import ClusterSerializer
