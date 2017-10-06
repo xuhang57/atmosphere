c113a7009d634fcacfcfff97447df0976b0aca9e
Lucas H. Xu <xuh@bu.edu>
2017-10-06 15:01:43 -0400

    workaround

--- api/v2/views/sahara_cluster.py
+++ api/v2/views/sahara_cluster.py
@@ -1,5 +1,5 @@
 import time
-from heatclient.common import template_utils
+#from heatclient.common import template_utils
 from api.v2.views.base import AuthViewSet, AuthOptionalViewSet
 
 from core.models import AtmosphereUser, Identity
@@ -74,7 +74,7 @@ class ClusterViewSet(AuthViewSet):
             else:
             	raise Exception("Cannot find an image for the plugin")
         image_id = image.id
-
+        '''
         files, heat_template = template_utils.process_template_path("/opt/dev/atmosphere/{plugin}-template.yml".format(plugin=plugin_name))
 
         heat_template['parameters']['image']['default'] = str(image_id)
@@ -103,6 +103,8 @@ class ClusterViewSet(AuthViewSet):
             if not cluster_id:
                 raise Exception("No cluster_id")
         results = [{"id": cluster_id, "clusterName": name, "pluginName": plugin_name, "hadoop_version": hadoop_version, "stackID": stack_id}]
+        '''
+        results=[]
         return Response(results, status=status.HTTP_201_CREATED)
 
 
